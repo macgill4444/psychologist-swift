120664ef9416fafdf4a3aaa3d7aa1cbc3847e8dd
Macgill Davis <macgill4444@gmail.com>
2015-08-27 12:05:50 -0400

    changed from buttons to picker with many more options ! eye!

--- Psychologist/PyschologistViewController.swift
+++ Psychologist/PyschologistViewController.swift
@@ -8,13 +8,7 @@
 
 import UIKit
 
-class PsychologistViewController: UIViewController {
-    
-    
-    @IBAction func nothing(sender: UIButton) {
-        //sender could be self, sender, or nil
-        performSegueWithIdentifier("nothing", sender: nil)
-    }
+class PsychologistViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
     
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
@@ -25,15 +19,71 @@ class PsychologistViewController: UIViewController {
         if let hvc = destination as? HappinessViewController {
             if let identifier = segue.identifier {
                 switch identifier {
-                case "Show Sad Face": hvc.happiness = 5
-                case "Show Meh Face": hvc.happiness = 52
-                case "Show Happy Face": hvc.happiness = 95
-                case "nothing": hvc.happiness = 25
+                case "enter": hvc.happiness = setHappiness()
                 default:break
                 }
             }
         }
     }
+    
+    var dreams: [String] = ["Duncan the Doge", "Winter", "Another ham sandwich", "Nothing", "Cheese", "A Million Bucks", "The Sox"]
+    
+    
+    @IBOutlet weak var dreamPicker: UIPickerView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Connect data:
+        self.dreamPicker.delegate = self
+        self.dreamPicker.dataSource = self
+        
+    }
+
+    
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return dreams.count
+    }
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return dreams[row]
+    }
+    
+    
+    @IBAction func enterDream() {
+        performSegueWithIdentifier("enter", sender: nil)
+    }
+    
+    func setHappiness() -> Int {
+        if let selectedRow = dreamPicker?.selectedRowInComponent(0) {
+            if let selectedDream = self.pickerView(dreamPicker, titleForRow: selectedRow, forComponent: 0) {
+                println("\(selectedDream)")
+                switch selectedDream {
+                case "Duncan the Doge":
+                    return 100
+                case "Winter":
+                    return 5
+                case "Another ham sandwich":
+                    return 53
+                case "Nothing":
+                    return 48
+                case "Cheese":
+                    return 95
+                case "A Million Bucks":
+                    return 88
+                case "The Sox":
+                    return 0
+                default:break
+                }
+            }
+        }
+        return 50
+    }
+    
 
 }
 
